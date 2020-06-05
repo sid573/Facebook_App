d0ceb365f7e5f36b5524ade137f50ce4a27aa180
nikitakhatriya <nikitakhatriya@gmail.com>
2020-06-05 21:32:29 +0530

    database schema

--- src/database/schema.js
+++ src/database/schema.js
@@ -3,5 +3,60 @@
     {
         { _id: "PSID", _name: ""},
     }
-    
+
+SHOP:[{ 
+	S_id: <id>;
+	name: 'name';
+
+    CUSTOMERS:													
+	{																	
+		c_id: <object1id>,													
+		name:"name",														
+		email:"email",														
+	};																	
+	PRODUCTS:[
+	{
+		category:[
+			category_id: <id>,
+			category_name: 'cat',
+			products:[
+				{
+					p_id: <id>,
+					p_name: 'name',
+					price: 'RSxx',
+					status: 'Y/N';
+				}
+			]
+		]
+	}
+	],
+	ORDERS: [
+	{
+		order_id: <id>,
+		customer: c_id,
+		address: 'add',
+		items_ordered: [
+			{
+				p_id1: p_id1;
+				quantity :'XX',
+			},
+			{
+				p_id2: p_id2;
+				quantity :'XX',
+			},
+		],
+		total: 'RSXXX',
+	}
+	}
+	]
+
+	TIME_SLOT: [
+	{
+		day: 'day',
+		time: 'time',
+		user_id: 'user',
+	}
+	],
+}
+];
 */
